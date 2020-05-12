5a98c0634180a59ecc054e3874c86067268a2dc4
Winfield Chen <wca88@sfu.ca>
2020-05-12 15:49:07 -0700

    Replace asserts with error logic

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stddef.h>
-#include <assert.h>
 #include <unistd.h>
 #include <getopt.h>
+#include <errno.h>
 #include "map/maps.h"
 int main(int argc, char ** argv) {
     enum {
@@ -39,10 +39,25 @@ int main(int argc, char ** argv) {
             default:
                 break;
         }
-        assert(opt_code != '?');
+        if (opt_code == '?') {
+            if (input_file != NULL) {
+                fclose(input_file);
+            }
+            if (output_file != NULL) {
+                fclose(output_file);
+            }
+            fputs("Error parsing arguments\n", stderr);
+            return EINVAL;
+        }
     } while (opt_code != -1);
-    assert(input_file != NULL);
-    assert(output_file != NULL);
+    if (input_file == NULL) {
+        fputs("No such input file\n", stderr);
+        return ENOENT;
+    }
+    if (output_file == NULL) {
+        fputs("Error opening output file\n", stderr);
+        return EBADF;
+    }
     for (Map_e map = MAP_0X; map <= MAP_22; map += 1) {
         printf("%d %p\n", map, get_map_p(map));
     }
