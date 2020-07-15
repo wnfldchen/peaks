eacfb8018bd1f036aab6a805592fa5656eccf520
Winfield Chen <wca88@sfu.ca>
2020-07-14 20:01:58 -0700

    Refactor p-value field pointers for heap

--- heap/heap.c
+++ heap/heap.c
@@ -74,16 +74,14 @@ void max_heapify(struct heap * const heap, size_t const i) {
     size_t const l = 2 * i + 1;
     size_t const r = 2 * i + 2;
     size_t m = i;
-    if (l < n &&
-        *(double *)get_format_field(heap->format, heap->array[l], P)
-        >
-        *(double *)get_format_field(heap->format, heap->array[m], P)) {
+    double const * const p_l = get_format_field(heap->format, heap->array[l], P);
+    double const * p_m = get_format_field(heap->format, heap->array[m], P);
+    double const * const p_r = get_format_field(heap->format, heap->array[r], P);
+    if (l < n && *p_l > *p_m) {
         m = l;
+        p_m = p_l;
     }
-    if (r < n &&
-        *(double *)get_format_field(heap->format, heap->array[r], P)
-        >
-        *(double *)get_format_field(heap->format, heap->array[m], P)) {
+    if (r < n && *p_r > *p_m) {
         m = r;
     }
     if (m != i) {
@@ -112,10 +110,9 @@ void make_heaps() {
 
 void filter_heap(struct heap * const heap, size_t const i) {
     size_t p = (i - 1) / 2;
-    if (i > 0 &&
-        *(double *)get_format_field(heap->format, heap->array[i], P)
-        >
-        *(double *)get_format_field(heap->format, heap->array[p], P)) {
+    double const * const p_i = get_format_field(heap->format, heap->array[i], P);
+    double const * const p_p = get_format_field(heap->format, heap->array[p], P);
+    if (i > 0 && *p_i > *p_p) {
         size_t const temp = heap->array[p];
         heap->array[p] = heap->array[i];
         heap->array[i] = temp;
@@ -128,10 +125,9 @@ void delete_heap(struct heap * const heap, size_t const i) {
     if (i < heap->len) {
         heap->array[i] = heap->array[heap->len];
         if (heap->len > 1) {
-            if (i == 0 ||
-                *(double *)get_format_field(heap->format, heap->array[i], P)
-                <
-                *(double *)get_format_field(heap->format, heap->array[(i - 1) / 2], P)) {
+            double const * const p_i = get_format_field(heap->format, heap->array[i], P);
+            double const * const p_ip = get_format_field(heap->format, heap->array[(i - 1) / 2], P);
+            if (i == 0 || *p_i < *p_ip) {
                 max_heapify(heap, i);
             } else {
                 filter_heap(heap, i);
